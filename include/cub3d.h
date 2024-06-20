3a8e18861269ae0c51566f19ee4f9fe24c0088a9
fcorvaro <filippo.corvaro.fc@gmail.com>
2024-06-20 19:20:05 +0200

    aggiunto un main di prova e il .h

---
+++ include/cub3d.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 19:09:34 by fcorvaro          #+#    #+#             */
+/*   Updated: 2024/06/20 19:18:07 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+/* ------------  LIBRARIES INCLUDED  ---------------------------------------- */
+
+# include "../lib/libftprintf/include/ft_printf.h"
+# include "../lib/mlx_linux/mlx.h"
+# include "../lib/mlx_linux/mlx_int.h"
+
+#endif
